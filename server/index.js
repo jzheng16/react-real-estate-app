200e5897b9c8c3986e90755dc6e927531fdd7846
Joey Zheng <jzheng16@binghamton.edu>
2019-01-06 04:35:46 -0500

    fix: path to build file

--- server/index.js
+++ server/index.js
@@ -63,15 +63,13 @@ app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '..', 'client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'client/build', 'index.html'));
   });
 }
-
-
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
